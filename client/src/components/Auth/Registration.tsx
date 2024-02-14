3ef400001adf799acc6453bca43307cacdbc6944
VladaNazarova <vldppvvtlvn@gmail.com>
2024-02-14 21:30:20 +0000

    styles main and auth

--- client/src/components/Auth/Registration.tsx
+++ client/src/components/Auth/Registration.tsx
@@ -145,7 +145,7 @@ export default function Registration() {
           <div className="py-4">
             <button
               type="submit"
-              className="flex w-full justify-center rounded-md bg-cyan-900 px-3 p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-900"
+              className="flex w-full justify-center rounded-md bg-cyan-900 px-3 p-2 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-cyan-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-cyan-700"
             >
               Sign up
             </button>
